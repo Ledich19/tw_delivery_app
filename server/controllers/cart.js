84b0ccba125e66d861a5535f38e43a3560621f1a
Aleksandr <ledich19@gmail.com>
2023-05-26 23:26:36 +0300

    feat: add cart api delete and get

--- server/controllers/cart.js
+++ server/controllers/cart.js
@@ -2,11 +2,22 @@ const Cart = require("../models/cartModel");
 
 const cartRouter = require("express").Router();
 
+shopRouter.get("/", async (request, response) => {
+  const shops = await Cart.find({});
+  response.json(shops);
+});
+
 cartRouter.post("/", async (request, response) => {
   const order = request.body;
   const newOrder = new Cart(order);
   const createdOrder = await newOrder.save();
   response.json(createdOrder);
+});
+
+shopRouter.delete("/:id", async (request, response) => {
+  const id = request.params.id;
+  await Cart.findByIdAndRemove(id);
+  response.status(204).end();
 });
 
 module.exports = cartRouter;
